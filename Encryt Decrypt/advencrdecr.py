3ce6211574422f0bba314ff66de8fe0d3bc1228e
husainnt <hussaintahir436@gmail.com>
2024-08-17 11:38:42 +0500

    New commit

--- Encryt Decrypt/advencrdecr.py
+++ Encryt Decrypt/advencrdecr.py
@@ -1,22 +1,18 @@
-#This is a password encrypter/decryptor made using python
+shift = 3
+
 action = input("What would you like to do? \nEncrypt\nDecrypt\n")
 if action == "Encrypt":
     message = input("Enter the message to encrypt: ")
     translated = ''
-    i = len(message) - 1
-    while i >= 0:
-        translated = translated + message[i]
-        i = i - 1
+    for char in message:
+        translated += chr(ord(char) + shift)
     msg = "Your encrypted message is: "
     print(msg + translated)
 elif action == "Decrypt":
-   
     message = input("Enter the message to decrypt: ")
     translated = ''
-    i = len(message) - 1
-    while i >= 0:
-        translated = translated + message[i]
-        i = i - 1
+    for char in message:
+        translated += chr(ord(char) - shift)
     msg = "Your decrypted message is: "
     print(msg + translated)
 else:
